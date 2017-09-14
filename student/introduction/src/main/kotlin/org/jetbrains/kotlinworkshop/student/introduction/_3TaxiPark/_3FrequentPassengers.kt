ba92e79578fd9738aa77508657cf538390bfc864
Moshe <moshe.waisberg@tikalk.com>
2017-09-14 16:30:17 +0300

    org/jetbrains/kotlinworkshop/student/introduction/_3TaxiPark/_3FrequentPassengers.kt

--- student/introduction/src/main/kotlin/org/jetbrains/kotlinworkshop/student/introduction/_3TaxiPark/_3FrequentPassengers.kt
+++ student/introduction/src/main/kotlin/org/jetbrains/kotlinworkshop/student/introduction/_3TaxiPark/_3FrequentPassengers.kt
@@ -2,4 +2,4 @@ package org.jetbrains.kotlinworkshop.student.introduction._3TaxiPark
 
 // Find all passengers that were taken by this driver more than once
 fun TaxiPark.findFrequentPassengers(driver: Driver): List<Passenger> =
-        TODO()
+        allPassengers.filter { p -> orders.filter { it.passengers.contains(p) }.count { it.driver == driver } > 1 }
