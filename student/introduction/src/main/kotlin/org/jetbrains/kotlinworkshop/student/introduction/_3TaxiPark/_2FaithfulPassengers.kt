b705a5733cafb09aef6e5d0cc7e74694986a3472
Moshe <moshe.waisberg@tikalk.com>
2017-09-14 16:27:24 +0300

    org/jetbrains/kotlinworkshop/student/introduction/_3TaxiPark/_2FaithfulPassengers.kt

--- student/introduction/src/main/kotlin/org/jetbrains/kotlinworkshop/student/introduction/_3TaxiPark/_2FaithfulPassengers.kt
+++ student/introduction/src/main/kotlin/org/jetbrains/kotlinworkshop/student/introduction/_3TaxiPark/_2FaithfulPassengers.kt
@@ -2,4 +2,4 @@ package org.jetbrains.kotlinworkshop.student.introduction._3TaxiPark
 
 // Find all passengers having more than 'minTrips' number of trips
 fun TaxiPark.findFaithfulPassengers(minTrips: Int): List<Passenger> =
-        TODO()
+        this.allPassengers.filter { p -> this.orders.count { it.passengers.contains(p) } > minTrips }
