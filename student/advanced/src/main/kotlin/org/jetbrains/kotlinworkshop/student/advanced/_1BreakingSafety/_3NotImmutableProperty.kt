bee6946981b1c37fdc17b617a7113df8a16f233f
Moshe <moshe.waisberg@tikalk.com>
2017-09-17 14:48:30 +0300

    org/jetbrains/kotlinworkshop/student/advanced/_1BreakingSafety/_3NotImmutableProperty.kt

--- student/advanced/src/main/kotlin/org/jetbrains/kotlinworkshop/student/advanced/_1BreakingSafety/_3NotImmutableProperty.kt
+++ student/advanced/src/main/kotlin/org/jetbrains/kotlinworkshop/student/advanced/_1BreakingSafety/_3NotImmutableProperty.kt
@@ -1,7 +1,19 @@
 package org.jetbrains.kotlinworkshop.student.advanced._1BreakingSafety
 
+import java.util.*
+
 class MyClass {
     // Change the implementation of 'myProperty' to make it return different values each time.
     // You may add additional properties if you need.
-    val myProperty: Int = TODO()
+    val random = Random()
+    var prevProperty: Int = 0
+    val myProperty: Int
+        get() {
+            var nextProperty: Int = random.nextInt()
+            while (prevProperty == nextProperty) {
+                nextProperty = random.nextInt()
+            }
+            prevProperty = nextProperty
+            return nextProperty
+        }
 }
